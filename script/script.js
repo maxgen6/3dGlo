50980c09e563e9545a2bec8bf21113d8c51819de
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-04-27 00:37:39 +0300

    дз19

--- script/script.js
+++ script/script.js
@@ -47,4 +47,55 @@ window.addEventListener('DOMContentLoaded', function(){
     // setInterval(countTimer, 1000, '27 april 2020');
 
 
+    //меню
+    const toggleMenu = () => {
+
+        const btnMenu = document.querySelector('.menu'),
+            menu = document.querySelector('menu'),
+            closeBtn = document.querySelector('.close-btn'),
+            menuItems = menu.querySelectorAll('ul>li');
+            
+        const handlerMenu = () => {
+            // if(!menu.style.transform || menu.style.transform === `translate(-100%) `){
+            //     menu.style.transform = `translate(0)`;
+            // } else{
+            //     menu.style.transform = `translate(-100%)`;
+            // }
+            menu.classList.toggle('active-menu');
+        };
+        btnMenu.addEventListener('click', handlerMenu);
+        closeBtn.addEventListener('click', handlerMenu);
+
+        menuItems.forEach((elem) => {
+            elem.addEventListener('click', handlerMenu)
+        });
+
+
+    };
+
+    toggleMenu();
+
+
+    //popup
+
+    const togglePopUp = () => {
+        const popup = document.querySelector('.popup'),
+            popUpBtn = document.querySelectorAll('.popup-btn'),
+            popUpClose = document.querySelector('.popup-close');
+
+        popUpBtn.forEach((elem) => {
+            elem.addEventListener('click', () => {
+                popup.style.display = 'block';
+            });
+        });
+
+        popUpClose.addEventListener('click', () => {
+            popup.style.display = 'none';
+        })
+
+    };
+
+    togglePopUp();
+
+
 });
