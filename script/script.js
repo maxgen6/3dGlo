e3bd9dc4fad314918bd67b297cc7895f94d134ad
Maksim Genyush <maxlindrgen0799@mail.ru>
2020-05-01 00:01:55 +0300

    дз23

--- script/script.js
+++ script/script.js
@@ -298,8 +298,15 @@ window.addEventListener('DOMContentLoaded', function(){
 
     //калькулятор
 
-    const calculator = () => {
+    const calculator = (price = 100) => {
         let calcItem = document.querySelectorAll('.calc-item');
+        
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
 
         for(let i = 1; i < calcItem.length; i++){
             calcItem[i].addEventListener('input', () => {
@@ -307,10 +314,47 @@ window.addEventListener('DOMContentLoaded', function(){
             });
         };
 
-        // console.log(calcItem);
+        const countSum = () => {
+            let total = 0,
+            countValue = 1,
+            dayValue = 1;
+
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+            
+            if(calcCount.value > 1){
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if(calcDay.value && calcDay.value < 5){
+                dayValue *= 2;
+            } else if(calcDay.value && calcDay.value < 10){
+                dayValue *= 1.5;
+            }
+
+            if(typeValue && squareValue){
+                total = price * typeValue * squareValue * countValue * dayValue;
+            };
+
+            totalValue.textContent = total;
+        };
+        
+        
+        
+        
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+            if(target.matches('.calc-type') || target.matches('.calc-square') || 
+                target.matches('.calc-day') || target.matches('.calc-count')){
+                    countSum()
+            }
+
+        })
+
+
 
 
     };
-    calculator();
+    calculator(100);
 
 });
